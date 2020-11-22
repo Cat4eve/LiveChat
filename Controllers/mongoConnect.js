fe4e0002a93ce52436815309193fe86ec2b35477
unknown <karapet.kh.2004@gmail.com>
2020-11-22 18:41:22 +0400

    dwadw

---
+++ Controllers/mongoConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { URL } = require('../config.json')
+
+mongoose.connect(URL, {useNewUrlParser: true});
+
+mongoose.connection.on(`connected`, ()=> {
+    // console.log(`Connected!`)
+    module.exports = true;
+})
+
+mongoose.connection.on(`error`, ()=> {
+    module.exports = false
+})
---
+++ Controllers/server.js
@@ -0,0 +1,31 @@
+const isConnected = require('./mongoConnect');
+
+if (isConnected) {
+    const koa = require('koa');
+    const { PORT } = require('../config.json');
+    const UserModel = require('../Models/Schemes/userSchema.js');
+
+    const app = new koa();
+
+    function createUser(username, email, password) {
+        return UserModel.create({
+            username: username,
+            email: email,
+            password: password
+        });
+    }
+
+    // app.use(async ctx => {
+    //     // ctx.body = 'Connected!';
+    //     let user1 = createUser('aaa', 'bbb', 'ccc')
+    //     console.log(user1);
+    // })
+
+    app.use(function(req, res) {
+        let user1 = createUser('aaa', 'bbb', 'ccc')
+        console.log(user1);
+      });
+      
+
+    app.listen(PORT)
+}
