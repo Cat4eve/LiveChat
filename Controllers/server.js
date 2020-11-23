02e91a058ab27f096eff88cc3edefa1418050e66
Cat4eve <karapet.kh.2004@gmail.com>
2020-11-23 15:29:49 +0400

    xd\ax

--- Controllers/server.js
+++ Controllers/server.js
@@ -4,10 +4,8 @@ if (isConnected) {
     const koa = require('koa');
     const { PORT } = require('../config.json');
     const UserModel = require('../Models/Schemes/userSchema.js');
-    const Router = require('@koa/router');
 
     const app = new koa();
-    const router = new Router();
 
     function createUser(username, email, password) {
         return UserModel.create({
@@ -18,22 +16,6 @@ if (isConnected) {
         });
     }
 
-    // app.use(function(ctx) {
-    //     if (ctx.url == '/'){
-    //         ctx.redirect('/login');
-    //     }
-    // });
-
-    router.get('/', (ctx)=>{
-        ctx.redirect('/login');
-    });
-
-    router.get('/login', (ctx)=>{
-        ctx.body = ''
-    });
-
-    app.use(router.routes());
-    app.use(router.allowedMethods());
 
     app.listen(PORT)
 }
