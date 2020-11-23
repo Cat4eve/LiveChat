7d1769063ac1a595c992fba674e556940bc5c382
Cat4eve <karapet.kh.2004@gmail.com>
2020-11-23 16:12:25 +0400

    a

--- Views/live-chat-front/src/app/login/login.component.ts
+++ Views/live-chat-front/src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+
+  _login = new FormGroup({
+    _emailOrUsername: new FormControl(''),
+    _password: new FormControl(''),
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginSubmit(): void {
+    console.log(this._login);
+
   }
 
 }
