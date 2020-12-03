ee786441ca62e23357dcf4e7d163cb56a67a8e0b
Hakob Aslanyan <hak.aslanyan47@gmail.com>
2020-12-03 15:15:54 +0400

    sd

--- Views/live-chat-front/src/app/login/login.component.ts
+++ Views/live-chat-front/src/app/login/login.component.ts
@@ -6,32 +6,61 @@ import { FormGroup, FormControl, NgForm, AbstractControl, ValidatorFn, Validator
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   encapsulation: ViewEncapsulation.None
-})
+}) 
 export class LoginComponent implements OnInit {
+
+  isEmailTrue: boolean;
+  isPasswordTrue: any; 
+  isCorrect: any;
 
   regWord = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   _login = new FormGroup({
     _email: new FormControl('', [Validators.required, this.emailValidator(this.regWord)]),
-    _password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-  })
+    _password: new FormControl('', [Validators.required, this.passwordValidator(/\d/)]),
+  }) 
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  emailValidator(nameRe: RegExp): ValidatorFn {
+  emailValidator(regex: RegExp): ValidatorFn {
     return (control: AbstractControl): {[key: string]: any} | null => {
-      const forbidden = nameRe.test(control.value);
-      console.log(forbidden);
+      const forbidden = regex.test(control.value);
+      this.isEmailTrue = forbidden;
       return forbidden ? {forbiddenName: {value: control.value}} : null;
     };
   }
 
+  passwordValidator(regex: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null;
+      }
+      this.isPasswordTrue = regex.test(control.value);
+      return this.isPasswordTrue;
+    };
+  }
+
   loginSubmit(): void {
-    console.log('adwad');
-    console.log(this._login);
+    if(this.isEmailTrue && this.isPasswordTrue){
+    //@ts-ignore
+     swal({
+        title: "Good job!",
+        text: "You are Loggined!",
+        icon: "success",
+        button: "Aww yiss!",
+      });
+    }else{
+    //@ts-ignore
+      swal({
+        title: "You cant Login",
+        text: "Enter correct email and password for login",
+        icon: "warning",
+        dangerMode: true,
+      })
+  }
   }
 
 }
